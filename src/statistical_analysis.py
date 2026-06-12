7a4fc39c382ee222341c9a81919dadc42af7d7bc
alicee127 <123898414+alicee127@users.noreply.github.com>
2026-06-12 12:27:01 +0200

    Add files via upload

--- src/statistical_analysis.py
+++ src/statistical_analysis.py
@@ -13,17 +13,17 @@ class CausalEffectAnalyzer():
     def compute_tce(self, treatment: str) -> float:
         return self.compute_cka(treatment) - self.compute_cka('control')
 
-    def t_tests(self, treatment: str):
-        treatment_arr = self.df[treatment].to_numpy()
-        control_arr = self.df['control'].to_numpy()
+    def t_tests(self, treatment: str, country: str):
+        treatment_arr = self.df[f'{treatment}_score'].to_numpy()
+        control_arr = self.df['control_score'].to_numpy()
 
         t_val, p_val = stats.ttest_ind(treatment_arr, control_arr, equal_var =False)
 
         alpha = 0.05
 
-        if np.abs(p_val) <= alpha:
-            print('Null hypothesis rejected. Significant statistical difference')
-        else:
-            print('Null hypothesis accepted. No significant statistical difference')
-        
+        significance = 'SIGNIFICANT STATISTICAL DIFFERENCE' if np.abs(p_val)<=alpha else 'NO SIGNIFICANT STATISTICAL DIFFERENCE'
+
+        print(f'{country} | {treatment} | t = {t_val:.3f} | p = {p_val:.3f} | {significance}')
+
+
         return t_val, p_val
